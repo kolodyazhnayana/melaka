731430a8db9129663ec78198107f3ed2864fb001
Yana <kolodyazhnaya19@yandex.ru>
2020-07-12 17:29:01 +0300

    menu

--- script.js
+++ script.js
@@ -3,7 +3,19 @@ document.getElementById("menu_burger").onclick = function() {
 };
 function open() {
     document.getElementById("menu_mobile").classList.toggle("menu_active");
-    document.getElementById("content").classList.toggle("content_active")
+    document.getElementById("content").classList.toggle("content_active");
     document.getElementById("menu_burger").classList.toggle("menu_burger_active");
 };
+
+document.getElementById("menu__list").onclick = function() {
+    close()
+};
+function close() {
+    document.getElementById("menu_mobile").classList.remove("menu_active");
+    document.getElementById("content").classList.remove("content_active");
+    document.getElementById("menu_burger").classList.remove("menu_burger_active");
+};
+
+
+
 
