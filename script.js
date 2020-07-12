52dceb2ef481f1b5525c9c2ce88cbb931f1bec93
Yana <kolodyazhnaya19@yandex.ru>
2020-07-12 18:13:14 +0300

    menu_mobile

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 document.getElementById("menu_burger").onclick = function() {
     open()
 };
+
 function open() {
     document.getElementById("menu_mobile").classList.toggle("menu_active");
     document.getElementById("content").classList.toggle("content_active");
     document.getElementById("menu_burger").classList.toggle("menu_burger_active");
 };
 
-document.getElementById("menu__list").onclick = function() {
-    close()
-};
-function close() {
-    document.getElementById("menu_mobile").classList.remove("menu_active");
-    document.getElementById("content").classList.remove("content_active");
-    document.getElementById("menu_burger").classList.remove("menu_burger_active");
-};
 
+document.getElementById("menu__list").querySelectorAll('.menu__link').forEach(item => {
+    item.onclick  = function() {
+        open()
+    };
+});
 
 
 
